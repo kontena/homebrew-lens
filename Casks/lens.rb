1081a525a6dc8ec0bbd2a36376c1aa561c003beb
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-10-30 16:06:50 +0200

    Add url/homepage difference comment

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -2,6 +2,7 @@ cask 'lens' do
   version '2.3.2'
   sha256 'e95f0a13a288c15466fbeda91f0fca7aa355c58ab2de7684686fe6e3f77ca916'
 
+  # kontena-lens-desktop.s3-eu-west-1.amazonaws.com was verified as official when first introduced to the cas
   url "https://kontena-lens-desktop.s3-eu-west-1.amazonaws.com/Lens-#{version}.dmg"
   name 'Lens'
   homepage 'https://kontena.io/'
