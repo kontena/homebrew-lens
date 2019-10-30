3b474fa40f25f9070dc7af4c463b7cb05430bea7
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-10-30 16:04:00 +0200

    Stylings

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,15 +1,18 @@
 cask 'lens' do
-  version "2.3.2"
-  sha256 "e95f0a13a288c15466fbeda91f0fca7aa355c58ab2de7684686fe6e3f77ca916"
+  version '2.3.2'
+  sha256 'e95f0a13a288c15466fbeda91f0fca7aa355c58ab2de7684686fe6e3f77ca916'
+
   url "https://kontena-lens-desktop.s3-eu-west-1.amazonaws.com/Lens-#{version}.dmg"
-  appcast "https://kontena-lens-desktop.s3-eu-west-1.amazonaws.com/latest-mac.yml"
-  name "Kontena Lens"
-  homepage "https://kontena.io/"
-  app "Lens.app"
+  name 'Lens'
+  homepage 'https://kontena.io/'
+
+  app 'Lens.app'
+
   uninstall delete: [
-              "~/Library/Caches/Lens"
-            ],
-            trash: [
-              "~/Library/Application Support/Lens",
+                      '~/Library/Caches/Lens',
+                    ]
+
+  uninstall trash: [
+              '~/Library/Application Support/Lens',
             ]
 end
