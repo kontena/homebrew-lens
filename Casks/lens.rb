f8dd85224e115fae38540daa123d30ae4187a482
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-10-30 15:36:28 +0200

    Which was wrong

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,6 +1,6 @@
 cask 'lens' do
-  version :latest
-  sha256 "76faa47ad4fcfe42001dc2921fc4aff04112db37cfb5c56e3ed35925ed61f43e04d391347264d749b00c96dff2c3d5bebb09ba53e5f3ee59113a078e7afc12e1"
+  version "2.3.2"
+  sha256 "e95f0a13a288c15466fbeda91f0fca7aa355c58ab2de7684686fe6e3f77ca916"
   url "https://kontena-lens-desktop.s3-eu-west-1.amazonaws.com/Lens-2.3.2.dmg"
   name "Kontena Lens"
   homepage "https://kontena.io/"
