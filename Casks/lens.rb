c3f4be6ee2b6954650c80b57e8e729f410540c17
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-10-30 15:34:50 +0200

    Add current sha256

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,11 +1,12 @@
 cask 'lens' do
   version :latest
-  sha256 :no_check
-  url 'https://kontena-lens-desktop.s3-eu-west-1.amazonaws.com/Lens-2.3.2.dmg'
-  name 'Kontena Lens'
-  homepage 'https://kontena.io'
-  app 'Lens.app'
+  sha256 "76faa47ad4fcfe42001dc2921fc4aff04112db37cfb5c56e3ed35925ed61f43e04d391347264d749b00c96dff2c3d5bebb09ba53e5f3ee59113a078e7afc12e1"
+  url "https://kontena-lens-desktop.s3-eu-west-1.amazonaws.com/Lens-2.3.2.dmg"
+  name "Kontena Lens"
+  homepage "https://kontena.io/"
+  app "Lens.app"
   uninstall delete: [
-    '~/Library/Application Support/Lens'
+    "~/Library/Application Support/Lens",
+    "~/Library/Caches/Lens"
   ]
 end
